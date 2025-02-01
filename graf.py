7f7a9b9f05764676a0abb3facd8d9420407c783a
mil-py <mytechnicalbox@gmail.com>
2025-02-01 12:38:01 +0300

    graf changed

--- graf.py
+++ graf.py
@@ -1,11 +1,10 @@
 from pprint import pprint
 
-from numpy.ma.core import count
-
 ''' табло номеронабирателя. 
     Задача - обойти конем, 
     найти маршрут с максимальным числом ходов '''
 
+#граф -{ вершина:[связанные вершины]}
 gr = {
     '1': ['6', '8'],
     '2': ['7', '9'],
@@ -22,21 +21,31 @@ gr = {
 #найти все пути
 def paths(gr):
     paths=[]
-    for node in gr:
-        paths.append([node])
-    lens = [len(val) for val in gr.values()]
-    count = max(list(lens))+3
-    print('count=',count)
-    while count >=0:
+
+    for node in gr:#добавляем стартовые точки
+        if gr[node]: #исключаем несвязанные вершины
+            paths.append([node])
+
+    while True:
         new_paths=[]
         for path in paths:
-            last = path[-1]
+            last = path[-1]#последняя вершина в пути
+            path_closed = True
             for node in gr[last]:
-                if not node in path:
+                if not node in path:#добавляем вершину если такой не было в данном пути
                     new_paths.append(path + [node])
-        paths = new_paths.copy()
-        print(count)
-        count-=1
+                    path_closed = False
+            if path_closed:# если ничего не добавилось, оставляем старый путь
+                new_paths.append(path)
+        if paths == new_paths:
+            break# если ничего не меняется - выход
+        else:
+            paths = new_paths.copy()
+
+
     return paths
 
-pprint(paths(gr))
+if __name__ == '__main__':
+    pprint(sorted(paths(gr)))
+    print('paths len =', len(paths(gr)))
+    print('max path len = ',max([len(path) for path in paths(gr)]))
